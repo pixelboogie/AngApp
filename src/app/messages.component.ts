9cc2319934f129de72d0cb8b0b4a20b80ed93a01
Shane Anderson <shane@pixelboogie.com>
2019-09-04 14:25:02 -0700

    Initial
    asdfafsd

---
+++ src/app/messages.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core'
+
+@Component({
+    selector: 'messages',
+    //template: 'this is the message {{messages[0].text}}'
+    template: 'this is the messages component <div *ngFor="let message of messages">{{message.text}} by: {{message.owner}}</div>'
+
+})
+
+export class MessagesComponent {
+    messages = [{text: 'some text', owner: 'Tim'}, {text: 'other message', owner: 'Jane'}];
+}
